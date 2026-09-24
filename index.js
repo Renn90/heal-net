274b34bd8b26f0b512e715c31a8026ab3b5aa237
Renn90 <106999299+Renn90@users.noreply.github.com>
2026-09-24 14:41:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// let student = {
+//     name: "David Adele",
+//     level: 300,
+//     cgpa: 4.2,
+//     Department: "Mass Communication"
+// }
+
+// if (student.level < 400 && student.cgpa >= 4.5) {
+//     console.log("You are a first class student, keep it up")
+// } else if (student.level >= 400 && student.cgpa >= 4.5) {
+//     console.log("Congrats!. You finished with a first class")
+// } else if (student.level >= 400 && student.cgpa < 4.5) {
+//     console.log("You finished below a first class, pursue another degree")
+// } else {
+//     console.log("Student is not active")
+// }
+
+// student.Department === "Mass Communication" ? console.log("This is a communicator") : console.log("This is not one")
+
+// const day = "Monday";
+
+// switch (day) {
+//   case "Monday":
+//     console.log("Start of the week");
+//     break;
+
+//   case "Friday":
+//     console.log("Almost weekend");
+//     break;
+
+//   case "Sunday":
+//     console.log("Weekend");
+//     break;
+
+//   default:
+//     console.log("Another day");
+// }
